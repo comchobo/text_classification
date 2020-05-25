7489edb1ca339e9c73fee52a9d7c5bab26ffea31
comchobo <65698076+comchobo@users.noreply.github.com>
2020-05-25 16:26:34 +0900

    Update Diary.py

--- Diary.py
+++ Diary.py
@@ -18,7 +18,6 @@ class diary:
     def __init__(self):
         self.model = load_model('Project_model.h5',custom_objects={'SeqSelfAttention':SeqSelfAttention})
         self.token = Tokenizer(9482)
-        #self.slicer = ['.', '?', '!']
         self.mc = Komoran()
         with open('wordIndex.json') as json_file:
             word_index = json.load(json_file)
@@ -27,11 +26,9 @@ class diary:
     def predict(self, paragraph):
         emotions = [0,0,0,0,0,0]
         sentences = paragraph.split('\n')
-        #finalize()
         import Model
         for sentence in sentences:
             output = Model.put(self.token, sentence, self.mc) #문장 토크나이즈
             emotion = Model.out(output, self.model) #토큰화된 문장을 감정으로 출력
-            emotions[emotion[0]]=emotions[emotion[0]]+1
-            #감정에 따라 감정 리스트에 각 감정점수를 플러스
+            emotions[emotion[0]]=emotions[emotion[0]]+1 #감정에 따라 감정 리스트에 각 감정점수를 플러스
         return emotions

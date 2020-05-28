6fc0acee4bf6f6ed6d7aef122756bbdda13b7fb7
comchobo <65698076+comchobo@users.noreply.github.com>
2020-05-28 17:27:17 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,7 @@ diary = Diary.diary() #모델 로드 및 형태소분석기 로드
 print("문단을 입력해보세요")
 para=input()
 print("슬픔, 중립, 행복, 불안, 분노, 예외 리스트입니다.")
-try:
-    response = diary.predict(para)
-    print(response)
-except:
-    print("error occurred") # exception not rethrown 예외처리
+response = diary.predict(para)
+print(response)
+#print("error occurred")  exception not rethrown 예외처리 (보류)
  
